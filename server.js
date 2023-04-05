56a72ccb53cd50b65bc7300fef9d9ce342fc33c8
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-04-05 12:45:39 +0000

    commit ajax setup backend

---
+++ server.js
@@ -0,0 +1,19 @@
+function loadDoc(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function(){
+        const resp = JSON.parse(this.responseText);  
+        document.getElementById('root').innerHTML = `
+         
+        <h4> HI </h4> 
+          
+        `; 
+    }
+
+    xhttp.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=3s6G8Ik06ynnWE9qbX1m1qIyyUwFF6ABjN69kwdk');
+    xhttp.send();
+
+}
+
+
+
+window.onload = () => loadDoc(); 
