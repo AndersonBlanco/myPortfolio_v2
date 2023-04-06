b496e8da302d33cd220c6a9a0791262fc09dc800
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-04-06 12:19:53 +0000

    r2

--- server.js
+++ server.js
@@ -2,19 +2,10 @@
 
 const script1 = document.createElement('script');
 script1.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js';
-const script2 = document.createElement('script');
-script2.src = 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.151.3/three.min.js';
-//GOD. 
+
 
 $('#header').load('./components/header.html'); 
 $('#center').load('./components/center.html'); 
-
-
-
-//THREE JS
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera();
 
 
 
